7c078e3f5c20423eba7dd889b367fe1b69d7d6b8
clalil <cliljander@gmail.com>
2019-08-22 20:04:05 +0200

    Refractors fizz_buzz method

--- lib/fizz_buzz.rb
+++ lib/fizz_buzz.rb
@@ -2,13 +2,17 @@
 def fizz_buzz(number)
     if (number).is_a? String
         'please enter a number'
-    elsif number % 15 == 0
+    elsif has_zero_remainder(number, 15)
         'fizz buzz'
-    elsif number % 5 == 0
+    elsif has_zero_remainder(number, 5)
         'buzz'
-    elsif number % 3 == 0
+    elsif has_zero_remainder(number, 3)
         'fizz'   
     else
       number
     end 
   end
+
+  def has_zero_remainder(number, divider)
+    number % divider == 0
+  endcd 
