70873f84aef25111e81dee39321650ab87a7dda1
clalil <cliljander@gmail.com>
2019-08-23 10:18:16 +0200

    Trying out further refractoring without avail

--- lib/fizz_buzz.rb
+++ lib/fizz_buzz.rb
@@ -1,8 +1,10 @@
 
 def fizz_buzz(number)
-    if (number).is_a? String
-        'please enter a number'
-    elsif has_zero_remainder(number, 15)
+    check_if_number(number)
+    # if number.is_a? Integer
+    # if (number).is_a? String
+    #     'please enter a number'
+    if has_zero_remainder(number, 15)
         'fizz buzz'
     elsif has_zero_remainder(number, 5)
         'buzz'
@@ -16,3 +18,11 @@ def fizz_buzz(number)
   def has_zero_remainder(number, divider)
     number % divider == 0
   end
+
+  def check_if_number(number)
+    if number.is_a? String
+     'Please enter an Integer'
+    end
+  end
+
+  fizz_buzz(5)
